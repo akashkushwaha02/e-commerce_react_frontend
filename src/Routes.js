1ae6c50ebc1d998483555c266ba54e2d7c672069
akash <akash.kushwaha02@gmail.com>
2021-03-02 16:12:16 +0530

    last commit of the frontend

--- src/Routes.js
+++ src/Routes.js
@@ -6,8 +6,7 @@ import Home from './core/Home';
 import Signin from './user/Signin';
 import Signup from './user/Signup'
 import UserDashboard from './user/UserDashboard';
-
-
+import Cart from './core/Cart'
 
 const Routes = () =>{
     return(
@@ -16,6 +15,7 @@ const Routes = () =>{
                 <Route path="/" exact component={Home} />                
                 <Route path="/signup" exact component={Signup} />     
                 <Route path="/signin" exact component={Signin} />    
+                <Route path="/cart" exact component={Cart} />    
                 <PrivateRoutes path="/user/dashboard" exact component={UserDashboard} />
             </Switch>
         </BrowserRouter>
--- src/core/Card.js
+++ src/core/Card.js
@@ -1,88 +1,91 @@
-import React from 'react'
-import { Redirect } from 'react-router-dom';
-import { addItemToCart,removeItemFromCart } from './helper/cartHelper';
-import ImageHelper from './helper/ImageHelper';
-
-
-
-const isAuthenticated = true
+import React, { useState } from "react";
+import ImageHelper from "./helper/ImageHelper";
+import { Redirect } from "react-router-dom";
+import { addItemToCart, removeItemFromCart } from "./helper/cartHelper";
+import { isAuthenticated } from "../auth/helper";
 
 const Card = ({
-    product,
-    addtoCart = true,
-    removeFromCart = true,
+  product,
+  addtoCart = true,
+  removeFromCart = false,
+  reload = undefined,
+  setReload = (f) => f,
+  // function(f){return f}
 }) => {
-    
-    const cartTitle = product ? product.name : "An alternate text";
-    const cartDescription = product ? product.description : "An alernate text"
-    const cartPrice = product ? product.price : "An alernate text"
+  const [redirect, setRedirect] = useState(false);
 
-    const addToCart = () => {
-      if (isAuthenticated) {
-        addItemToCart(product, () => {});
-        console.log("Added to cart");
-      } else {
-        console.log("Login Please!");
-      }
-    };
+  const cartTitle = product ? product.name : "A photo from pexels";
+  const cartDescription = product ? product.description : "Default description";
+  const cartPrice = product ? product.price : "Default";
 
-    const getAredirect = redirect => {
-      if(redirect){
-        return <Redirect to="/cart" />
-      }
+  const addToCart = () => {
+    if (isAuthenticated()) {
+      addItemToCart(product, () => setRedirect(true));
+      console.log("Added to cart");
+    } else {
+      console.log("Login Please!");
     }
+  };
 
-    const showAddToCart = (addToCart) => {
-      return (
-          addtoCart && (
-            <button
-              onClick={addToCart}
-              className="btn btn-block btn-outline-success mt-2 mb-2">
-              Add to Cart
-          </button>
-          )
-      );
-    };
+  const getAredirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
 
-    const showRemoveFromCart = removeFromCart => {
-      return (
-        removeFromCart && (
-          <button
-          //Todo: need to rectify this error
+  const showAddToCart = (addToCart) => {
+    return (
+      addtoCart && (
+        <button
+          onClick={addToCart}
+          className="btn btn-block btn-outline-success mt-2 mb-2"
+        >
+          Add to Cart
+        </button>
+      )
+    );
+  };
+
+  const showRemoveFromCart = (removeFromCart) => {
+    return (
+      removeFromCart && (
+        <button
           onClick={() => {
-            removeItemFromCart(product.id)
-            console.log("Product has been removed")
+            //TODO: handle this too
+            removeItemFromCart(product.id);
+            setReload(!reload);
+
+            console.log("Product removed from cart");
           }}
           className="btn btn-block btn-outline-danger mt-2 mb-2"
         >
           Remove from cart
         </button>
-        )
       )
-    }
-
-    return (
-      <div className="card text-white bg-dark border border-info ">
-        <div className="card-header lead">{cartDescription}</div>
-        <div className="card-body">
-            
-        <ImageHelper product={product} />
-
-          <p className="lead bg-success font-weight-normal text-wrap">
-            {cartTitle}
-          </p>
-          <p className="btn btn-success rounded  btn-sm px-4">Rs. {cartPrice}</p>
-          <div className="row">
-            <div className="col-12">
-              {showAddToCart(addToCart)}
-            </div>
-            <div className="col-12">
-              {showRemoveFromCart(removeFromCart)}
-            </div>
-          </div>
-        </div>
-      </div>
     );
   };
 
-  export default Card;
+  return (
+    <div className="card text-white bg-dark border border-info ">
+      <div className="card-header lead">{cartTitle}</div>
+      <div className="card-body">
+        {getAredirect(redirect)}
+        <ImageHelper product={product} />
+        <p className="lead bg-success font-weight-normal text-wrap">
+          {cartDescription}
+        </p>
+        <p className="btn btn-success rounded  btn-sm px-4">$ {cartPrice}</p>
+        <div className="row">
+          <div className="col-12">
+            {showAddToCart(addToCart)}
+          </div>
+          <div className="col-12">
+            {showRemoveFromCart(removeFromCart)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
